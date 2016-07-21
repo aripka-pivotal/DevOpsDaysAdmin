3caa7ac5790d36376a5b386f4cc28f85ceaaedab
Andrew Ripka <aripka@pivotal.io>
2016-07-21 13:22:40 -0500

    add the random winner picker

--- src/main/java/io/pivotal/devopsdays/repository/DrawingRegistrantRepository.java
+++ src/main/java/io/pivotal/devopsdays/repository/DrawingRegistrantRepository.java
@@ -1,5 +1,6 @@
 package io.pivotal.devopsdays.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import io.pivotal.devopsdays.entity.DrawingRegistrant;
@@ -9,5 +10,8 @@ import java.util.List;
 public interface DrawingRegistrantRepository extends CrudRepository<DrawingRegistrant, Long> {
 
 	public List<DrawingRegistrant> findByTicketNumber(String ticketnumber);
+	
+	@Query(nativeQuery=true, value="select * from Drawing_Registrant ORDER BY RAND() LIMIT 1")
+	public DrawingRegistrant getWinner();
  
 }
