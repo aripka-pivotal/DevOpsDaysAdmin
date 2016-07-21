ec26fbc085d123707e79df11c70b24dd842fa3c1
Andrew Ripka <aripka@pivotal.io>
2016-07-20 19:33:23 -0500

    downoad data as CSV for later use

--- src/main/java/io/pivotal/devopsdays/web/CSVDownloadController.java
+++ src/main/java/io/pivotal/devopsdays/web/CSVDownloadController.java
@@ -1,13 +1,15 @@
 package io.pivotal.devopsdays.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import io.pivotal.devopsdays.entity.DrawingRegistrant;
 import io.pivotal.devopsdays.repository.DrawingRegistrantRepository;
@@ -19,16 +21,28 @@ public class CSVDownloadController {
 	DrawingRegistrantRepository repo;
 	
 	
-	@RequestMapping(value = "/csv")
-	public void downloadCSV(HttpServletResponse response) throws IOException{
+	@RequestMapping(value = "/downloadCSV")
+	public void downloadCSV(HttpServletResponse response) throws IOException {
+
+		String headerKey = "Content-Disposition";
+		String csvFileName = "registrants.csv";
+		String headerValue = String.format("attachment; filename=\"%s\"",
+				csvFileName);
+				response.setHeader(headerKey, headerValue);
+
+		response.setContentType("text/csv");
 		
-		PrintWriter pw = response.getWriter();
-		pw.println("name,company,emailAddress");
-		for(DrawingRegistrant dw: repo.findAll()){
-			pw.println(dw.getName()+","+dw.getCompany()+","+dw.getEmailAddress());;
+		// uses the Super CSV API to generate CSV data from the model data
+		ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(),
+		CsvPreference.STANDARD_PREFERENCE);
+		
+		String[] header = { "name", "company", "emailAddress", };
+		
+		csvWriter.writeHeader(header);
+		for (DrawingRegistrant drawingRegistrant: repo.findAll()) {
+		csvWriter.write(drawingRegistrant, header);
 		}
 		
-
-
+		csvWriter.close();
 	}
 }
